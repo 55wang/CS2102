9070ef8dd72439511a4457ad2b0bea8dcaab3375
Wang Zhe <wangzhe.lynx@gmail.com>
2015-02-26 03:13:27 +0800

    clean build

--- src/main/java/Uploads.java
+++ src/main/java/Uploads.java
@@ -21,11 +21,13 @@ import javax.servlet.http.Part;
 public class Uploads extends HttpServlet {
  
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
     out.println("post");
     
   }
  
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    PrintWriter out = response.getWriter();
     out.println("get");
   }
  
